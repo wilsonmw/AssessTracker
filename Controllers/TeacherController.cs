b2b02681f41261302973da13bbad717b00587f86
Matt Wilson <matthewwarrenwilson@gmail.com>
2017-11-16 11:50:37 -0800

    Added dashboard and logout links to all pages

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -26,6 +26,7 @@ namespace AssessTracker.Controllers
             if(HttpContext.Session.GetInt32("Permission") == null){
                 return RedirectToAction("Index", "Home");
             }
+            ViewBag.permission = HttpContext.Session.GetInt32("Permission");
             return View("addTeacherPage");
         }
 
@@ -54,6 +55,37 @@ namespace AssessTracker.Controllers
             _context.Teachers.Add(newTeacher);
             _context.SaveChanges();
             return RedirectToAction("dashboard", "Dashboard");
+        }
+
+        [HttpGet]
+        [Route("deleteTeacherPage")]
+        public IActionResult deleteTeacherPage(){
+            List<Teacher> allTeachers = _context.Teachers.OrderBy(l => l.LastName).ToList();
+            ViewBag.allTeachers = allTeachers;
+            return View("deleteTeacherPage");
+        }
+
+        [HttpPost]
+        [Route("deleteTeacher")]
+        public IActionResult deleteTeacher(int teacher){
+            List<Kid> allKids = _context.Kids.Include(t => t.Teacher).ToList();
+            int count = 0;
+            foreach (var kid in allKids){
+                if(kid.TeacherId == teacher){
+                    count += 1;
+                }
+            }
+            if(count > 0){
+                List<Teacher> allTeachers = _context.Teachers.OrderBy(l => l.LastName).ToList();
+                ViewBag.allTeachers = allTeachers;
+                Teacher thisTeach = _context.Teachers.SingleOrDefault(t => t.id == teacher);
+                ViewBag.teacherError = thisTeach.Prefix + " " + thisTeach.LastName + " cannot be removed at this time because there are still children listed as being in their class. If " + thisTeach.Prefix + " " + thisTeach.LastName + " is no longer on staff, please go through and update the children to reflect the new teacher before removing this teacher.";
+                return View("deleteTeacherPage");
+            }
+            Teacher deleteTeach = _context.Teachers.SingleOrDefault(t => t.id == teacher);
+            _context.Teachers.Remove(deleteTeach);
+            _context.SaveChanges();
+            return RedirectToAction("addTeacherPage");
         }
     }
 } 
